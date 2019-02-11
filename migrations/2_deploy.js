40d64ec61755fbf7d70d85da39df27c1434cfb3d
Jonathan Passerat-Palmbach <j.passerat-palmbach@imperial.ac.uk>
2019-02-11 09:38:16 +0000

    fix: asssign exising address to Enigma deployment

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -3,7 +3,8 @@ var Enigma = artifacts.require("Enigma");
 
 module.exports = async function(deployer, network, accounts) {
     deployer.then(async () => {
-        enigma = await deployer.deploy(Enigma, "0x0", accounts[0])
+        // FIXME set second param (ENG address) to real value, here mock to allow deployment while unused
+        enigma = await deployer.deploy(Enigma, accounts[0], accounts[0])
         await deployer.deploy(Voting, enigma.address);
     })
 };
